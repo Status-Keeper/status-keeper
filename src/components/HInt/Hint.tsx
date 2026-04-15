fbcddde09e103bed135c4ebd007f9a91cc0fe62b
artyom_ryzhkov <a.b.ryzhkov@gmail.com>
2026-04-15 23:36:07 +0300

    NONE: added capitalization

--- src/components/HInt/Hint.tsx
+++ src/components/HInt/Hint.tsx
@@ -4,12 +4,17 @@ interface Props {
 }
 
 export function Hint({ title, hints }: Props) {
+	function capitalize(str: string): string {
+		const value = str.trim();
+
+		return String(value.trim()).charAt(0).toUpperCase() + String(value).slice(1)
+	}
 
 	return (
 		<div>
 			<div><b>{title ? title : "💡 ИИ Советы по приемке:"}</b></div>
 			<ul>
-				{hints.map(hint => (<li>{hint}</li>))}
+				{hints.map(hint => (<li>{capitalize(hint)}</li>))}
 			</ul>
 		</div>
 	)
