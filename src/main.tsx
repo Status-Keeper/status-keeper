251523382f8718137b6f30df2034d0f9cde6a019
artyom_ryzhkov <a.b.ryzhkov@gmail.com>
2026-04-15 20:59:49 +0300

    NONE: LPs were removed

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router';
-import App from './App.tsx'
-import './index.css'
-import { LandingPage } from './pages/LandingPage/LandingPage.tsx';
+
+import App from './App.tsx';
+import './index.css';
+
 import { NotFoundPage } from './pages/NotFoundPage/NotFoundPage.tsx';
 import { ManagerPage } from './pages/ManagerPage/ManagerPage.tsx';
 import { CookiesUsagePage, PersonalDataPolicyPage, PersonalDataProcessingPage, ReceiveNewslettersPage } from './pages/Privacy';
-import { LandingPage2 } from './pages/LandingPageV2/LP2.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -19,8 +19,6 @@ createRoot(document.getElementById('root')!).render(
         <Route path='receive-newsletters' element={<ReceiveNewslettersPage />} />
         <Route path='status' element={<App />} />
         <Route path='manager' element={<ManagerPage />} />
-        <Route path='lp' element={<LandingPage />} />
-        <Route path='lp-new' element={<LandingPage2 />} />
         <Route path='/*' element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
--- src/pages/LandingPageV2/SectionShell.tsx
+++
@@ -1,9 +0,0 @@
-export function SectionShell({
-	children,
-	className = '',
-}: {
-	children: React.ReactNode
-	className?: string
-}) {
-	return <section className={`panel ${className}`.trim()}>{children}</section>
-}
