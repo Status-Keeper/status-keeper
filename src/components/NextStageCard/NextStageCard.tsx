f28db5f4599845197f391f0dbacb293f0195da38
artyom_ryzhkov <a.b.ryzhkov@gmail.com>
2026-06-14 15:28:57 +0300

    NONE: Hide suggest for not existed hints

--- src/components/NextStageCard/NextStageCard.tsx
+++ src/components/NextStageCard/NextStageCard.tsx
@@ -12,20 +12,33 @@ export function NextStageCard({ stage, hint }: Props) {
 		return null;
 	}
 
+	const hints = hint ? hint.split(';') : [];
+
 	return (
-		<section className="next-stage-card">
-			<div>
-				<div className="next-stage-info">
-					<div>
-						<div className="next-stage-label">Следующий этап</div>
-						<div className="next-stage-title">{stage.title}</div>
-						{
-							stage.nextStageHint &&
-							<Hint title={<span>💡 Как подготовиться<sup>*</sup></span>} hints={hint ? hint.split(';') : []}></Hint>
-						}
+		<div>
+			<section className="next-stage-card">
+				<div>
+					<div className="next-stage-info">
+						<div>
+							<div className="next-stage-label">Следующий этап</div>
+							<div className="next-stage-title">{stage.title}</div>
+							{
+								stage.nextStageHint &&
+								<Hint title={<span>💡 Как подготовиться<sup>*</sup></span>} hints={hints}></Hint>
+							}
+						</div>
 					</div>
 				</div>
-			</div>
-		</section>
+			</section>
+
+			{
+				hints.length > 0 &&
+				<div className='fyi'>
+					* <i>Советы сгенерированы искусственным интеллектом. Они могут быть неточными, поэтому всегда уточняйте ключевые моменты у вашей ремонтной бригады</i>
+				</div>
+			}
+
+		</div>
+
 	);
 }
